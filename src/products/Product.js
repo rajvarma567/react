9c3bd918cb38c7b0e76f669848fcf0c23e3d3a07
rajvarma567 <rajvarma.vivartha@gmail.com>
2019-06-18 17:01:13 +0530

    changes

--- src/products/Product.js
+++ src/products/Product.js
@@ -17,7 +17,7 @@ export class Product extends React.Component{
                                        this.props.click(this.props.data);
 
                                     }
-                                }>Details</button>
+                                }>Detailsssssssss</button>
                          <input type="number" onChange={(event)=> {
   this.props.onChange(event.target.value);
                  }
